212236723e92d2e85c921bda6b2370dfdcb8d5c4
Timothy-py <adeyeyetimothy33@gmail.com>
2023-11-19 20:54:24 +0100

    ft<ExceptionHandler>:included global exception handler middleware

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from middlewares.exceptions import ExceptionHandlerMiddleware
 from routes.tasks import task_api_router
 
 app = FastAPI()
 
 
 app.include_router(task_api_router)
+app.add_middleware(ExceptionHandlerMiddleware)
---
+++ middlewares/exceptions.py
@@ -0,0 +1,18 @@
+from fastapi import Request, HTTPException
+from fastapi.responses import JSONResponse
+from starlette.middleware.base import BaseHTTPMiddleware
+
+class ExceptionHandlerMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        try:
+            return await call_next(request)
+        except HTTPException as http_exception:
+            return JSONResponse(
+                status_code=http_exception.status_code,
+                content={"error": "Client Error", "message": str(http_exception.detail)},
+            )
+        except Exception as e:
+            return JSONResponse(
+                status_code=500,
+                content={"error": "Internal Server Error", "message": "An unexpected error occurred."},
+            )
