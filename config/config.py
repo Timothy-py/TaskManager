f94f2c22f18190b4da3b63e1abbd44c08f95584a
Timothy-py <adeyeyetimothy33@gmail.com>
2023-11-19 16:16:44 +0100

    ft<config:env>:setup environment variables module

---
+++ config/config.py
@@ -0,0 +1,16 @@
+from functools import lru_cache
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    DB_URI: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
+
+@lru_cache
+def get_settings():
+    return settings
+
+env_vars = get_settings()
