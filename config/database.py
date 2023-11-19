a9566ecb8dea57bf096c1a9e1149a65af1898f20
Timothy-py <adeyeyetimothy33@gmail.com>
2023-11-19 18:51:01 +0100

    ft<DB:Mongo>:setup and connected MongoDB

---
+++ config/database.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+from config.config import env_vars
+ 
+# Create a new client and connect to the server
+client = MongoClient(env_vars.DB_URI, server_api=ServerApi('1'))
+
+mongo_client = client.TaskManager
+
+db = mongo_client["tasks"]
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
