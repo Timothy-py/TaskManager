3feecab6964a0e79d48cc7acd74135aa29b32f4f
Timothy-py <adeyeyetimothy33@gmail.com>
2023-11-28 22:45:02 +0100

    fx<Route>:code optimize

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi_events.middleware import EventHandlerASGIMiddleware
 from fastapi_events.handlers.local import local_handler
 from contextlib import asynccontextmanager
-# from middlewares.exceptions import ExceptionHandlerMiddleware
+from middlewares.exceptions import ExceptionHandlerMiddleware
 from routes.tasks import task_api_router
 from event.handlers import handle_task_update
 from config.kafka import kafka_manager
@@ -17,7 +17,7 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-app.include_router(task_api_router, prefix="/api/v1/tasks")
+app.include_router(task_api_router)
 app.add_middleware(EventHandlerASGIMiddleware, handlers=[local_handler])
 app.add_middleware(ExceptionHandlerMiddleware)
 
--- routes/tasks.py
+++ routes/tasks.py
@@ -2,34 +2,34 @@ from fastapi import APIRouter, Body, status
 from services.tasks import TaskService
 from schemas.tasks import TaskCreate, TaskResponse, TaskUpdate
 
-task_api_router = APIRouter()
+task_api_router = APIRouter(prefix="/api/v1/tasks", tags=["Task"])
 
 
 # GET A TASK DETAILS
-@task_api_router.get("/", tags=["Task"], response_model=TaskResponse, status_code=status.HTTP_200_OK)
+@task_api_router.get("/", response_model=TaskResponse, status_code=status.HTTP_200_OK)
 async def get_task(id: str):
     return TaskService.get_task(id)
 
 
 # CREATE TASK
-@task_api_router.post("/", tags=["Task"], response_model=TaskResponse, status_code=status.HTTP_201_CREATED)
+@task_api_router.post("/", response_model=TaskResponse, status_code=status.HTTP_201_CREATED)
 async def create_task(task_data: TaskCreate = Body(...)):
     return TaskService.create_task(task_data)
 
 
 # GET ALL TASKS
-@task_api_router.get("/all", tags=["Task"], response_model=list[TaskResponse], status_code=status.HTTP_200_OK)
+@task_api_router.get("/all", response_model=list[TaskResponse], status_code=status.HTTP_200_OK)
 async def get_all_tasks():
     return TaskService.get_all_tasks()
 
 
 # UPDATE TASK STATUS
-@task_api_router.patch("{id}/status", tags=["Task"], response_model=TaskResponse, status_code=status.HTTP_200_OK)
+@task_api_router.patch("{id}/status", response_model=TaskResponse, status_code=status.HTTP_200_OK)
 async def update_task_status(id: str, status: TaskUpdate = Body(...)):
     return TaskService.update_task_status(id, status)
 
 
 # DELETE TASK ITEM
-@task_api_router.delete("{id}", tags=["Task"], status_code=status.HTTP_204_NO_CONTENT)
+@task_api_router.delete("{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_task(id: str):
     return TaskService.delete_task(id)
